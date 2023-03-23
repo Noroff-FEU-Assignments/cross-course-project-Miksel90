0ffd024ba42a3178fd8dbebf644fff642131e82c
Miksel90 <114400071+Miksel90@users.noreply.github.com>
2023-03-23 13:53:01 +0100

    da

--- js/register.js
+++ js/register.js
@@ -2,7 +2,7 @@
 const form = document.querySelector("form");
 const email = document.querySelector("#email");
 const emailError = document.querySelector("#emailError");
-const button = document.querySelector("btn-3");
+const button = document.querySelector("reg-button");
 
 
 
