80e967e5dcf314faf0d7fc1460ec7526e50a97cf
Miksel90 <114400071+Miksel90@users.noreply.github.com>
2023-04-12 12:24:43 +0200

    so much js

--- js/featured.js
+++ js/featured.js
@@ -1,17 +1,54 @@
-// Set the URL of your WordPress site
-const apiUrl = "http://rainydays.local/wp-json/wc/store/products";
+const endpointUrl = "http://rainydays.local/wp-json/wc/store/products";
 
-// Set the endpoint for the product data
-const endpoint = 'products';
+const productGrid = document.getElementById('productGrid');
+const featuredContainer = document.getElementById('featuredProducts');
+const productDetailContainer = document.getElementById('productDetail');
 
-// Fetch the product data from the API
-fetch(apiUrl + endpoint)
-  .then(response => response.json())
-  .then(data => {
-    // Process the data as needed
-    console.log(data);
-  })
-  .catch(error => {
-    // Handle any errors
-    console.error(error);
-  });
+async function fetchProducts() {
+    try {
+        const response = await fetch(endpointUrl); 
+        const products = await response.json();
+
+        // Showing only the last 4 products
+        const lastFourProducts = products.slice(-4);
+        
+        // Rendering last 4 product data as thumbnails in a grid
+        lastFourProducts.forEach(product => {
+            const thumbnail = document.createElement('div');
+            thumbnail.classList.add('thumbnail');
+            // Render product name and price in thumbnail
+            thumbnail.innerHTML = `<h3>${product.name}</h3><p>Price: ${product.price}</p><img src="${product.images[0].src}" alt="${product.name}">`;
+            productGrid.appendChild(thumbnail);
+            
+            // Filtering and rendering featured products in their own section
+            if (product.featured) {
+                const featuredThumbnail = document.createElement('div');
+                featuredThumbnail.classList.add('thumbnail');
+                // Render featured product name and price in thumbnail
+                featuredThumbnail.innerHTML = `<h3>${product.name}</h3><p>Price: ${product.price}</p><img src="${product.images[0].src}" alt="${product.name}">`;
+                featuredContainer.appendChild(featuredThumbnail);
+            }
+            
+            // Adding click event listener to each product thumbnail to show product detail page
+            thumbnail.addEventListener('click', () => {
+                renderProductDetail(product);
+            });
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// Function to render product detail page
+function renderProductDetail(product) {
+    // Clear existing product detail
+    productDetailContainer.innerHTML = '';
+  
+    // Render product detail
+    const detail = document.createElement('div');
+    detail.classList.add('product-detail');
+    detail.innerHTML = `<h3>${product.name}</h3><p>Price: ${product.price}</p><img src="${product.images[0].src}" alt="${product.name}">`;
+    productDetailContainer.appendChild(detail);
+}
+
+fetchProducts();
