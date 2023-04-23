62e386675572ce39a15a7c26b98e0a041d1cf39b
Miksel90 <114400071+Miksel90@users.noreply.github.com>
2023-04-23 11:22:48 +0200

    Update featured.js

--- js/featured.js
+++ js/featured.js
@@ -3,3 +3,57 @@ const apiBase = "http://rainydays.local";
 const woocomBase = "/wp-json/wc/store";
 const productBase = "/products";
 
+// Full URL with query parameter
+const fullProductURL = apiBase + woocomBase + productBase + "?featured=true";
+
+// Fetching the products
+async function getProducts() {
+  const response = await fetch(fullProductURL);
+
+  const products = await response.json();
+
+  return products;
+}
+
+// Create HTML
+function createProductHTML(product) {
+  const container = document.querySelector(".container-featured");
+
+  const productContainer = document.createElement("div");
+  productContainer.classList.add("product");
+  productContainer.id = product.id;
+
+  const title = document.createElement("h2");
+  title.innerText = product.name;
+  productContainer.append(title);
+
+  for (let i = 0; i < product.images.length; i++) {
+    const imgData = product.images[i];
+    const img = document.createElement("img");
+    img.src = imgData.src;
+    img.alt = imgData.alt;
+    productContainer.append(img);
+  }
+
+  const price = document.createElement("p");
+  price.innerText = "Price: $" + (product.prices.price / 100).toFixed(2);
+  productContainer.append(price);
+
+  container.append(productContainer);
+}
+
+function createProductsHTML(products) {
+  for (let i = 0; i < products.length; i++) {
+    const product = products[i];
+    createProductHTML(product);
+  }
+}
+
+// Create the main function
+async function main() {
+  const products = await getProducts();
+  createProductsHTML(products);
+}
+
+// Run the entire function
+main();
