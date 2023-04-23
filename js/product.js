8a6640358d11134c9e209b960be42fa42054be35
Miksel90 <114400071+Miksel90@users.noreply.github.com>
2023-04-23 10:42:45 +0200

    Update product.js

--- js/product.js
+++ js/product.js
@@ -1,2 +1,15 @@
 const queryString = window.location.search;
 console.log(queryString);
+
+
+
+if(window.location.pathname.includes("/index.html")) {
+    main();
+}
+
+if(window.location.pathname.includes("/jacket-specific.html")) {
+    let queryString = window.location.search;
+    queryString = queryString.slice(1);
+    currentProductId = parseInt(queryString);
+    main();
+}
